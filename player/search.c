shraman-rc/ai-leiserchess

commit 233c7b69cca7d479b3345221228c541028768ea6

use pointers for victims

player/search.c
@@ -258,11 +258,12 @@ score_t searchRoot(position_t *p, score_t alpha, score_t beta, int depth,
     (*node_count_serial)++;
 
     // make the move.
-    victims_t x = make_move(&(rootNode.position), &(next_node.position), mv);
-
-    if (is_KO(x)) {
+    bool isko = make_move(&(rootNode.position), &(next_node.position), mv);
+    if (isko) {
       continue;  // not a legal move
     }
+
+    victims_t* x = &rootNode.position.victims;
 
     if (is_game_over(x, rootNode.pov, rootNode.ply)) {
       score = get_game_over_score(x, rootNode.pov, rootNode.ply);

player/leiserchess.c
@@ -151,7 +151,15 @@ victims_t make_from_string(position_t *old, position_t *p,
     }
   }
 
-  return (mv == 0) ? ILLEGAL() : make_move(old, p, mv);
+  if (mv == 0) {
+    return ILLEGAL();
+  } else {
+    if (make_move(old, p, mv)) {
+      return KO();
+    } else {
+      return p->victims;
+    }
+  }
 }
 
 typedef enum {

player/move_gen.c
@@ -521,7 +521,8 @@ square_t fire(position_t *p) {
 
 
 // return victim pieces or KO
-victims_t make_move(position_t *old, position_t *p, move_t mv) {
+// return whether is_KO
+bool make_move(position_t *old, position_t *p, move_t mv) {
   tbassert(mv != 0, "mv was zero.\n");
 
   WHEN_DEBUG_VERBOSE(char buf[MAX_CHARS_IN_MOVE]);
@@ -572,9 +573,9 @@ victims_t make_move(position_t *old, position_t *p, move_t mv) {
     p->victims.zapped = 0;
 
     if (USE_KO &&  // Ko rule
-        zero_victims(p->victims) &&
+        zero_victims(&p->victims) &&
         (p->key == (old->key ^ zob_color))) {
-      return KO();
+      return true;  // KO();
     }
   } else {  // we definitely hit something with laser
     p->victims.zapped = p->board[victim_sq];
@@ -592,7 +593,7 @@ victims_t make_move(position_t *old, position_t *p, move_t mv) {
       });
   }
 
-  return p->victims;
+  return false; // p->victims;
 }
 
 // helper function for do_perft
@@ -737,12 +738,12 @@ bool is_ILLEGAL(victims_t victims) {
       (victims.zapped == ILLEGAL_ZAPPED);
 }
 
-bool zero_victims(victims_t victims) {
-  return (victims.stomped == 0) &&
-      (victims.zapped == 0);
+bool zero_victims(victims_t* victims) {
+  return (victims->stomped == 0) &&
+      (victims->zapped == 0);
 }
 
-bool victim_exists(victims_t victims) {
-  return (victims.stomped > 0) ||
-      (victims.zapped > 0);
+bool victim_exists(victims_t* victims) {
+  return (victims->stomped > 0) ||
+      (victims->zapped > 0);
 }

player/move_gen.h
@@ -140,7 +140,7 @@ typedef struct position {
   piece_t      board[ARR_SIZE];
   struct position  *history;     // history of position
   uint64_t     key;              // hash key
-  uint16_t          ply;              // Even ply are White, odd are Black
+  uint16_t     ply;              // Even ply are White, odd are Black
   move_t       last_move;        // move that led to this position
   victims_t    victims;          // pieces destroyed by shooter or stomper
   square_t     kloc[2];          // location of kings
@@ -177,17 +177,19 @@ int generate_all(position_t *p, sortable_move_t *sortable_move_list,
                  bool strict);
 void do_perft(position_t *gme, int depth, int ply);
 square_t low_level_make_move(position_t *old, position_t *p, move_t mv);
-victims_t make_move(position_t *old, position_t *p, move_t mv);
+bool make_move(position_t *old, position_t *p, move_t mv);
 void display(position_t *p);
 uint64_t compute_zob_key(position_t *p);
 
 victims_t KO();
 victims_t ILLEGAL();
 
+// not used anywhere that speed matters
 bool is_ILLEGAL(victims_t victims);
 bool is_KO(victims_t victims);
-bool zero_victims(victims_t victims);
-bool victim_exists(victims_t victims);
+// use pointers
+bool zero_victims(victims_t* victims);
+bool victim_exists(victims_t* victims);
 
 void mark_laser_path(position_t *p, bool *laser_map, color_t c);
 #endif  // MOVE_GEN_H

player/search_common.c
@@ -82,11 +82,11 @@ static score_t get_draw_score(position_t *p, int ply) {
   uint64_t cur = p->key;
   score_t score;
   while (true) {
-    if (!zero_victims(x->victims)) {
+    if (!zero_victims(&x->victims)) {
       break;  // cannot be a repetition
     }
     x = x->history;
-    if (!zero_victims(x->victims)) {
+    if (!zero_victims(&x->victims)) {
       break;  // cannot be a repetition
     }
     if (x->key == cur) {  // is a repetition
@@ -115,11 +115,11 @@ static bool is_repeated(position_t *p, int ply) {
   uint64_t cur = p->key;
 
   while (true) {
-    if (!zero_victims(x->victims)) {
+    if (!zero_victims(&x->victims)) {
       break;  // cannot be a repetition
     }
     x = x->history;
-    if (!zero_victims(x->victims)) {
+    if (!zero_victims(&x->victims)) {
       break;  // cannot be a repetition
     }
     if (x->key == cur) {  // is a repetition
@@ -135,15 +135,15 @@ static bool is_repeated(position_t *p, int ply) {
 // check the victim pieces returned by the move to determine if it's a
 // game-over situation.  If so, also calculate the score depending on
 // the pov (which player's point of view)
-static bool is_game_over(victims_t victims, int pov, int ply) {
-  tbassert(ptype_of(victims.stomped) != KING, "Stomped a king.\n");
-  return ptype_of(victims.zapped) == KING;
+static bool is_game_over(victims_t* victims, int pov, int ply) {
+  tbassert(ptype_of(victims->stomped) != KING, "Stomped a king.\n");
+  return ptype_of(victims->zapped) == KING;
 }
 
-static score_t get_game_over_score(victims_t victims, int pov, int ply) {
-  tbassert(ptype_of(victims.stomped) != KING, "Stomped a king.\n");
+static score_t get_game_over_score(victims_t* victims, int pov, int ply) {
+  tbassert(ptype_of(victims->stomped) != KING, "Stomped a king.\n");
   // score negative when victims.zapped == WHITE
-  score_t score = -1*(1 - 2*(color_of(victims.zapped)))*WIN*pov;
+  score_t score = -1*(1 - 2*(color_of(victims->zapped)))*WIN*pov;
   return score + (1 - 2*(score >= 0))*ply;
 }
 
@@ -255,15 +255,16 @@ void evaluateMove(searchNode *node, move_t mv, move_t killer_a,
   result->next_node.parent = node;
 
   // Make the move, and get any victim pieces.
-  victims_t victims = make_move(&(node->position), &(result->next_node.position),
-                                mv);
+  bool isko = make_move(&(node->position), &(result->next_node.position), mv);
 
   // Check whether this move changes the board state.
   //   such moves are not legal.
-  if (is_KO(victims)) {
+  if (isko) {
     result->type = MOVE_ILLEGAL;
     return;
   }
+
+  victims_t* victims = &result->next_node.position.victims;
 
   // Check whether the game is over.
   if (is_game_over(victims, node->pov, node->ply)) {
@@ -286,18 +287,18 @@ void evaluateMove(searchNode *node, move_t mv, move_t killer_a,
     return;
   }
 
-  tbassert(victims.stomped == 0
-           || color_of(victims.stomped) != node->fake_color_to_move,
+  tbassert(victims->stomped == 0
+           || color_of(victims->stomped) != node->fake_color_to_move,
            "stomped = %d, color = %d, fake_color_to_move = %d\n",
-           victims.stomped, color_of(victims.stomped),
+           victims->stomped, color_of(victims->stomped),
            node->fake_color_to_move);
 
 
   // Check whether we caused our own piece to be zapped. This isn't considered
   //   a blunder if we also managed to stomp an enemy piece in the process.
-  if (victims.stomped == 0 &&
-      victims.zapped > 0 &&
-      color_of(victims.zapped) == node->fake_color_to_move) {
+  if (victims->stomped == 0 &&
+      victims->zapped > 0 &&
+      color_of(victims->zapped) == node->fake_color_to_move) {
     blunder = true;
   }
 

tests/gen_openings/gen_openings.c
@@ -152,7 +152,15 @@ victims_t make_from_string(position_t *old, position_t *p,
     }
   }
 
-  return (mv == 0) ? ILLEGAL() : make_move(old, p, mv);
+  if (mv == 0) {
+    return ILLEGAL();
+  } else {
+    if (make_move(old, p, mv)) {
+      return KO();
+    } else {
+      return p->victims;
+    }
+  }
 }
 
 typedef enum {

tests/gen_openings/new_gen_openings.c
@@ -150,7 +150,15 @@ victims_t make_from_string(position_t *old, position_t *p,
     }
   }
 
-  return (mv == 0) ? ILLEGAL() : make_move(old, p, mv);
+  if (mv == 0) {
+    return ILLEGAL();
+  } else {
+    if (make_move(old, p, mv)) {
+      return KO();
+    } else {
+      return p->victims;
+    }
+  }
 }
 
 typedef enum {

commit 95b7f83342b3133d7c613d5324245d7142dde3d8

Fix mistake introduced in 233c7b69cca7d47 -- victims from wrong position after make_move

player/search.c
@@ -263,7 +263,7 @@ score_t searchRoot(position_t *p, score_t alpha, score_t beta, int depth,
       continue;  // not a legal move
     }
 
-    victims_t* x = &rootNode.position.victims;
+    victims_t* x = &next_node.position.victims;
 
     if (is_game_over(x, rootNode.pov, rootNode.ply)) {
       score = get_game_over_score(x, rootNode.pov, rootNode.ply);